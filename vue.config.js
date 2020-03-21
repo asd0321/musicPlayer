fb51aed845c8f59a3a08ab967c3210446d645292
asd0321 <43774802+asd0321@users.noreply.github.com>
2020-03-21 21:59:21 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,5 @@
 module.exports={
-	devServer: {
-	    proxy:{
-	        '/api':{
-	            target:'http://localhost:8080',
-	            pathRewrite:{
-	                '^/api':'src/common/data'
-	            }
-	        }
-    	}
-    }
+	
+    
 
 }
